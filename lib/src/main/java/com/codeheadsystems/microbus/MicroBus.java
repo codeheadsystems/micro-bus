47655f02638c528c02193f0f17d9d64287943981
Ned Wolpert <ned.wolpert@gmail.com>
2026-04-11 12:58:09 -0700

    Cleaned up the messages more

--- lib/src/main/java/com/codeheadsystems/microbus/MicroBus.java
+++ lib/src/main/java/com/codeheadsystems/microbus/MicroBus.java
@@ -7,12 +7,9 @@ import java.util.concurrent.CopyOnWriteArrayList;
 import java.util.concurrent.ExecutorService;
 import java.util.concurrent.Executors;
 import java.util.function.Consumer;
-import javax.inject.Inject;
-import javax.inject.Singleton;
 import org.slf4j.Logger;
 import org.slf4j.LoggerFactory;
 
-@Singleton
 public class MicroBus<D> implements Bus<D> {
 
   private static final Logger LOGGER = LoggerFactory.getLogger(MicroBus.class);
@@ -22,29 +19,32 @@ public class MicroBus<D> implements Bus<D> {
   private final ContextFactory contextFactory;
   private final ExecutorService executorService;
 
-  @Inject
   public MicroBus(ContextFactory contextFactory) {
     this(contextFactory, Executors.newCachedThreadPool());
   }
 
   public MicroBus(ContextFactory contextFactory,
                   ExecutorService executorService) {
+    LOGGER.debug("MicroBus()");
     this.contextFactory = Objects.requireNonNull(contextFactory);
     this.executorService = Objects.requireNonNull(executorService);
   }
 
   @Override
   public void register(ContextFiller<D> filler) {
+    LOGGER.debug("register({})", filler);
     contextFillers.add(Objects.requireNonNull(filler));
   }
 
   @Override
   public void unregister(ContextFiller<D> filler) {
+    LOGGER.debug("unregister({})", filler);
     contextFillers.remove(filler);
   }
 
   @Override
   public void publish(Message<D> message) {
+    LOGGER.debug("publish({})", message.uuid());
     if (message.async()) {
       executorService.submit(() -> dispatch(message));
     } else {
@@ -54,19 +54,22 @@ public class MicroBus<D> implements Bus<D> {
 
   @Override
   public void subscribe(Consumer<Message<D>> subscriber) {
+    LOGGER.debug("subscribe({})", subscriber);
     subscribers.add(Objects.requireNonNull(subscriber));
   }
 
   @Override
   public void unsubscribe(Consumer<Message<D>> subscriber) {
+    LOGGER.debug("unsubscribe({})", subscriber);
     subscribers.remove(subscriber);
   }
 
   private void dispatch(Message<D> message) {
     contextFactory.withRunnable(() -> {
-      LOGGER.debug("[{}] Starting dispatching message {}", contextFactory.contextUuid(), message);
-      Context context = contextFactory.currentContext().orElseThrow();
-      Map<String, Object> properties = fillContext(message, context);
+      LOGGER.debug("[{}] dispatch({})", contextFactory.contextUuid(), message.uuid());
+      Map<String, Object> properties = contextFactory.currentContext()
+          .map(ctx ->  fillContext(message, ctx))
+          .orElseThrow();
       for (Consumer<Message<D>> subscriber : subscribers) {
         if (message.async()) {
           dispatchAsync(message, subscriber, properties);
@@ -74,44 +77,46 @@ public class MicroBus<D> implements Bus<D> {
           dispatchSync(message, subscriber);
         }
       }
-      LOGGER.debug("[{}] Finished dispatching message {}", contextFactory.contextUuid(), message);
+      LOGGER.debug("[{}] Finished dispatching message {}", contextFactory.contextUuid(), message.uuid());
     });
   }
 
   private void dispatchSync(final Message<D> message,
                             final Consumer<Message<D>> subscriber) {
-    LOGGER.debug("[{}] dispatching message {} synchronously to {}", contextFactory.contextUuid(), message.uuid(), subscriber);
-    try {
-      subscriber.accept(message);
-    } catch (Exception e) {
-      LOGGER.error("Subscriber threw exception for message {}: {}", message.uuid(), e.getMessage(), e);
-    }
+    LOGGER.trace("[{}] dispatching message {} synchronously to {}", contextFactory.contextUuid(), message.uuid(), subscriber);
+    callSubscriber(message, subscriber);
   }
 
   private void dispatchAsync(final Message<D> message,
                              final Consumer<Message<D>> subscriber,
                              final Map<String, Object> properties) {
-    LOGGER.debug("[{}] dispatching message {} asynchronously to {}", contextFactory.contextUuid(), message, subscriber);
+    LOGGER.trace("[{}] dispatching message {} asynchronously to {}", contextFactory.contextUuid(), message, subscriber);
     executorService.submit(() -> {
       contextFactory.withRunnable(() -> {
         contextFactory.currentContext().ifPresent(ctx -> ctx.properties().putAll(properties));
-        try {
-          subscriber.accept(message);
-        } catch (Exception e) {
-          LOGGER.error("Subscriber threw exception for message {}: {}", message.uuid(), e.getMessage(), e);
-        }
+        callSubscriber(message, subscriber);
       });
     });
   }
 
+  private void callSubscriber(final Message<D> message,
+                              final Consumer<Message<D>> subscriber) {
+    try {
+      subscriber.accept(message);
+    } catch (Exception e) {
+      LOGGER.error("[{}] Subscriber threw exception for message {}: {}", contextFactory.contextUuid(), message.uuid(), subscriber, e);
+    }
+  }
+
   private Map<String, Object> fillContext(final Message<D> message,
                                           final Context context) {
+    LOGGER.debug("[{}] fillContext({})", contextFactory.contextUuid(), message.uuid());
     for (ContextFiller<D> filler : contextFillers) {
       try {
-        LOGGER.debug("[{}] filling context from {}", contextFactory.contextUuid(), filler);
+        LOGGER.trace("[{}] filling context from {} for {}", contextFactory.contextUuid(), filler, message.uuid());
         filler.fillContext(message.data(), context);
       } catch (Exception e) {
-        LOGGER.error("ContextFiller threw exception for message {}: {}", message.uuid(), e.getMessage(), e);
+        LOGGER.error("ContextFiller threw exception for message {}: {}", message.uuid(), filler, e);
       }
     }
     return context.properties();
